dd53c2c129bedc3401cef29ff24d8b9bf0fc32ac
kosmos107 <strateg87@gmail.com>
2021-12-08 17:22:49 +0300

    делаем бургер

---
+++ src/components/header/Menu/Burger.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from "./Menu.module.scss"
+
+
+const Burger = ()=> {
+    
+    return (
+        <div className={s.Burger__wrapper}>
+            <span className={s.Burger__line}></span>
+        </div>
+    )
+}
+
+export default Burger
--- src/components/header/Menu/Menu.tsx
+++ src/components/header/Menu/Menu.tsx
@@ -1,16 +1,21 @@
 import Controls from "./Controls"
 import s from "./Menu.module.scss"
 import Search from "./Search"
+import Burger from "./Burger"
 const logo = "https://static-sl.insales.ru/files/1/2355/16312627/original/logo.svg"
-console.log(logo)
+
+
 
 const Menu = ()=>{
-
+        
+     
     return (
         <div className={s.Menu__wrapper}>
-            <img className={s.Menu__logo}  src={logo} />
+            <Burger />
+            <a href="3" className={s.Menu__logo}><img src={logo}/></a>
             <Search/>
             <Controls/>
+        
         </div>
     )
 }
