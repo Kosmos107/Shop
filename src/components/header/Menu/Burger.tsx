de86c3e5346e89744a690f22c35a2b1e304b1c70
kosmos107 <strateg87@gmail.com>
2021-12-10 17:22:16 +0300

    готовый бургер

--- src/components/header/Menu/Burger.tsx
+++ src/components/header/Menu/Burger.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import s from "./Menu.module.scss"
+import cn from "classnames"
+
+// const n = `${s.Burger__wrapper} ${s.Burger__active}`
 
 
-const Burger = ()=> {
+interface Burg{
+    change:()=>void,
+    active:boolean,
+    
+}
+
+const Burger:React.FC<Burg>= (props)=> {
+    console.log(props)
+    const classBurger = cn(s.Burger__line,{[s.Burger__active]:props.active})
+    
+    
     
     return (
-        <div className={s.Burger__wrapper}>
-            <span className={s.Burger__line}></span>
+        <div onClick={props.change} className={s.Burger__wrapper}>
+            <span className={classBurger}></span>
         </div>
     )
 }
--- src/components/header/Menu/Menu.tsx
+++ src/components/header/Menu/Menu.tsx
@@ -1,17 +1,24 @@
+import React,{useState} from "react"
 import Controls from "./Controls"
 import s from "./Menu.module.scss"
 import Search from "./Search"
 import Burger from "./Burger"
 const logo = "https://static-sl.insales.ru/files/1/2355/16312627/original/logo.svg"
 
-
+//сделать функцию для измерения размера экрана
 
 const Menu = ()=>{
-        
-     
+    const size:number = window.innerWidth
+    const [userSize,setUserSize] = useState(size)
+    const changeActive = ():void=>{
+        setActive((prev:boolean):boolean=>!prev)
+        console.log(active)
+    } 
+    const [active,setActive]=useState(false)
+     ///сделать адаптив через useEffect
     return (
         <div className={s.Menu__wrapper}>
-            <Burger />
+            <Burger  active={active} change={changeActive} />
             <a href="3" className={s.Menu__logo}><img src={logo}/></a>
             <Search/>
             <Controls/>
