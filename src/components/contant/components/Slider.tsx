6224b9b4789a143b333857c31a970aa084ddf3dc
kosmos107 <strateg87@gmail.com>
2022-01-04 18:35:52 +0300

    работа над слайдером 2.0

--- src/components/contant/components/Slider.tsx
+++ src/components/contant/components/Slider.tsx
@@ -1,47 +1,58 @@
-import React,{useState,useRef} from 'react'
+import React,{useState,useRef,useEffect} from 'react'
 import s from "../Contant.module.scss"
 import Product from "./Product"
 import cn from "classnames"
 import { AiOutlineArrowLeft,AiOutlineArrowRight} from "react-icons/ai";
+import { TypedSelector } from '../../../Hooks/TypedSelector'
 
+interface slider {
+    children?:React.ReactChild|React.ReactNode
+}
 
+const Slider:React.FC<slider> = ({children}) => {
+    const state = TypedSelector(state=>state.product.list)
+    useEffect(()=>{
+        console.log("change")
+    },[])
+    
 
+    const sliderRef = useRef<HTMLDivElement>(null)
+    const [leftArrow] = useState(false)
+    const [rigthArrow] = useState(false)
 
-const Slider = () => {
-
-    const slider = useRef<HTMLDivElement>(null)
     const prevHandler = ()=>{
-        console.log("prev")
-        if (slider.current !== null) {
-          
-          }
-       
+        if(sliderRef.current){
+            console.log(sliderRef)
+            console.log(sliderRef.current.scrollLeft)
+            sliderRef.current.scrollTo({
+                left:sliderRef.current.scrollLeft - 340,
+                behavior:"smooth"
+            })
+        }   
     }
     const nextHandler = () =>{
-        console.log("next")
-        // slider.current.childNodes.map(el)
+        if(sliderRef.current){
+            sliderRef.current.scrollTo({
+                left:sliderRef.current.scrollLeft+340,
+                behavior:"smooth"
+            })
+        }
     }
+
+    const arrowLeftCl = cn(s.slider__arrow,s.slider__prev,{[s.slider__active]:leftArrow})
+    const arrowRigthCl = cn(s.slider__arrow,s.slider__next,{[s.slider__active]:rigthArrow})
+   
     return (
     
         
         <div className={s.slider}>
-            <button onClick={prevHandler} className={cn(s.slider__arrow,s.slider__prev)}><AiOutlineArrowLeft/></button>
-            <button onClick={nextHandler} className={cn(s.slider__arrow,s.slider__next)}><AiOutlineArrowRight/></button>
-            <div className={s.slider__line} ref={slider}>
-                    <div>1</div>
-                    <div>2</div>
-                    <div>3</div>
-                    <div>4</div>
-                    <div>5</div>
-                    <div>6</div>
-                    <div>7</div>
-                    <div>8</div>
-                    <div>9</div>
-                    <div>10</div>
-                    <div>11</div>
-
+            <button onClick={prevHandler} className={arrowLeftCl}><AiOutlineArrowLeft/></button>
+            <button onClick={nextHandler} className={arrowRigthCl}><AiOutlineArrowRight/></button>
+            <div className={s.slider__line} ref={sliderRef}>
+                {state.slice(0,6).map((el)=>{
+                    return( <Product id={el.id} price={el.price} name={el.name}  width={"300px"} img={el.img} />)
+                })}
             </div>
-            
         </div>
     )
 }
